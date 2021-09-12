99f2aa567a1eeaa798c43f1d098a1b78680b6af3
Mariia <49761493+lavMary@users.noreply.github.com>
2021-09-12 17:07:57 +0300

    Create Wizard.java

---
+++ Wizard.java
@@ -0,0 +1,25 @@
+public class Wizard implements WizardAttack{
+    protected int wizardDamage;
+
+    public Wizard(int wizardDamage) {
+        this.wizardDamage = wizardDamage;
+    }
+
+    public int getWizardDamage() {
+        return wizardDamage;
+    }
+
+    public void setWizardDamage(int wizardDamage) {
+        this.wizardDamage = wizardDamage;
+    }
+
+    public void st(WizardAttack wizardAttack) {
+        wizardAttack.attack();
+
+    }
+
+    @Override
+    public void attack() {
+        System.out.println("I lose hp!");
+    }
+}
