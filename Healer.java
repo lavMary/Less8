784f185636c4d9be36ab81928061c26d075b9481
Mariia <49761493+lavMary@users.noreply.github.com>
2021-10-02 11:18:02 +0300

    Create Healer.java

---
+++ Healer.java
@@ -0,0 +1,20 @@
+public abstract class Healer extends Hero {
+    private int healPower;
+
+    public Healer(int hp, int level, int damage) {
+        super(hp,level,damage);
+    }
+
+    public int getHealPower() {
+        return healPower;
+    }
+
+    public void setHealPower(int healPower) {
+        this.healPower = healPower;
+    }
+
+    void heal() {
+        System.out.println("It`s ok. I`m healing!");
+    }
+    
+}
