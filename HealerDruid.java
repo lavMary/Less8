d1cd626c019207afc1b99b3ad348122ff4b095e6
Mariia <49761493+lavMary@users.noreply.github.com>
2021-10-02 11:18:22 +0300

    Create HealerDruid.java

---
+++ HealerDruid.java
@@ -0,0 +1,28 @@
+public class HealerDruid extends Healer implements Trainee, Info {
+    private Beast beast;
+    public HealerDruid (int hp, int level, int damage) {
+        super(hp, level, damage);
+    }
+
+    public Beast getBeast() {
+        return beast;
+    }
+
+    public void setBeast(Beast beast) {
+        this.beast = beast;
+    }
+
+    void tameBeast() {
+        System.out.println("The beast is tamed.");
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Hi! My name is Ella.");
+    }
+
+    @Override
+    public void train() {
+        System.out.println("Healer cant training here");
+    }
+}
